dde8a6da0b4f6d8da5af02527ca040db50a919ec
Vi Tran <77949259+ViTran25@users.noreply.github.com>
2022-12-29 19:23:48 -0500

    Add initial pseudocode

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Begin the game, the game will have 5 rounds	(game)
+// Begin each round	(playRound)
+// Generate computer's choice	(getComputerChoice)
+// Ask the user to choose a choice
+// Compare user's choice with computer's choice
+// If the user wins, give the user 1 point and declare the user win
+// If computer wins, give the computer 1 point and declare the user loose
+// Show the score board to the user
+// Move to next round if it hasn't reached 5 rounds
+// Finish the game and announce the winner
