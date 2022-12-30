db17a3a0ee6956bf6840392c8bf175c336995beb
Vi Tran <77949259+ViTran25@users.noreply.github.com>
2022-12-29 19:32:51 -0500

    Add getComputerChoice function
    This function will generate and return a choice for computer
    (rock, paper or scissors) randomly

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 // Begin the game, the game will have 5 rounds	(game)
 // Begin each round	(playRound)
-// Generate computer's choice	(getComputerChoice)
+// Generate computer's choice
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
+
 // Ask the user to choose a choice
 // Compare user's choice with computer's choice
 // If the user wins, give the user 1 point and declare the user win
